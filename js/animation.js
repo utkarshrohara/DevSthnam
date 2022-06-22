cb20db90d98e96305c2b72d00ac32f8f06d33f1d
Utkarsh Rohara <utkarshroharaur04@gmail.com>
2022-06-22 12:15:59 +0530

    .

---
+++ js/animation.js
@@ -0,0 +1,45 @@
+var words = ['#ॐ त्रयम्बके यजामहे सुगंधि पुष्टिवृध्नम उर्वारुक्मिव बन्धनान् मृत्योमुर्क्षीय मार्मतात्!!!'],
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 25,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.word').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
